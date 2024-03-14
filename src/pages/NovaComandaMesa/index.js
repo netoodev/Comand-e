24074de2c2a8e1e981e7a0b58b7cf7273bb5ea3b
Neto <netoodeveloper@gmail.com>
2024-03-14 00:39:49 -0300

    Nova Comanda page heading

--- src/pages/NovaComandaMesa/index.js
+++ src/pages/NovaComandaMesa/index.js
@@ -6,8 +6,13 @@ const NovaComandaMesa = () => {
     return(
         <>
             <Voltar/>
-            <div className='page-title'>
-                <h1>Nova Comanda</h1>
+            <div className='page-heading'>
+                <div className='page-title'>
+                    <h1>Nova Comanda</h1>
+                </div>
+                <div className='table-id'>
+                    <p>Mesa: 1</p>
+                </div>
             </div>
         </>
     );
